a869fcf43953073567bdfb32a65d8dce49fddca2
Jason Stewart <jason.r.stewart@gmail.com>
2016-11-17 10:03:58 -0600

    Add a create_from_normalized_vector function to the CoordinateSystem3 module
    Add a create_from_normalized_vector function that creates a coordinate
    system from a normalized vector. Update the create_from_vector function
    to perform a vector normalization before creating the coordinate system
    from a given vector.

--- lib/raytracer/coordinate_system_3.ex
+++ lib/raytracer/coordinate_system_3.ex
@@ -1,7 +1,4 @@
 defmodule Raytracer.CoordinateSystem3 do
-  @moduledoc """
-  """
-
   alias Raytracer.Vector3
 
   defstruct [
@@ -10,12 +7,15 @@ defmodule Raytracer.CoordinateSystem3 do
     w_axis: %Vector3{dx: 0.0, dy: 0.0, dz: 1.0},
   ]
 
-  @doc """
-  Creates a three-dimensional coordinate system from `vector`.
+  def create_from_vector(vector) do
+    vector
+    |> Vector3.normalize
+    |> create_from_normalized_vector
+  end
 
-  This function assumes that `vector` is normalized.
-  """
-  def create_from_vector(%Vector3{dx: dx, dy: dy, dz: dz} = vector) when abs(dx) > abs(dy) do
+  def create_from_normalized_vector(
+    %Vector3{dx: dx, dy: dy, dz: dz} = vector
+  ) when abs(dx) > abs(dy) do
     v_axis =
       %Vector3{dx: -dz, dy: 0.0, dz: dx}
       |> Vector3.divide(:math.sqrt(dx * dx + dz * dz))
@@ -23,7 +23,7 @@ defmodule Raytracer.CoordinateSystem3 do
     %__MODULE__{u_axis: vector, v_axis: v_axis, w_axis: w_axis}
   end
 
-  def create_from_vector(%Vector3{dy: dy, dz: dz} = vector) do
+  def create_from_normalized_vector(%Vector3{dy: dy, dz: dz} = vector) do
     v_axis =
       %Vector3{dx: 0.0, dy: dz, dz: -dy}
       |> Vector3.divide(:math.sqrt(dy * dy + dz * dz))
--- test/raytracer/coordinate_system_3_test.exs
+++ test/raytracer/coordinate_system_3_test.exs
@@ -6,9 +6,21 @@ defmodule Raytracer.CoordinateSystem3Test do
 
   describe "Raytracer.CoordinateSystem3.create_from_vector/1" do
     test "creates a coordinate system from a vector" do
-      v = %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
+      v = %Vector3{dx: 0.0, dy: 0.0, dz: 2.0}
 
       c = CoordinateSystem3.create_from_vector(v)
+
+      assert c.u_axis == %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
+      assert c.v_axis == %Vector3{dx: 0.0, dy: 1.0, dz: 0.0}
+      assert c.w_axis == %Vector3{dx: -1.0, dy: 0.0, dz: 0.0}
+    end
+  end
+
+  describe "Raytracer.CoordinateSystem3.create_from_normalized_vector/1" do
+    test "creates a coordinate system from a vector" do
+      v = %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
+
+      c = CoordinateSystem3.create_from_normalized_vector(v)
 
       assert c.u_axis == %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
       assert c.v_axis == %Vector3{dx: 0.0, dy: 1.0, dz: 0.0}
@@ -18,7 +30,7 @@ defmodule Raytracer.CoordinateSystem3Test do
     test "creates a coordinate system from a vector with dx larger than dy" do
       v = %Vector3{dx: 1.0, dy: 0.0, dz: 0.0}
 
-      c = CoordinateSystem3.create_from_vector(v)
+      c = CoordinateSystem3.create_from_normalized_vector(v)
 
       assert c.u_axis == %Vector3{dx: 1.0, dy: 0.0, dz: 0.0}
       assert c.v_axis == %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
