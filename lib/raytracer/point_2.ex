0a7a1b79cb4432c00641ab730043c0eab519a194
Jason Stewart <jason.r.stewart@gmail.com>
2016-11-19 19:45:24 -0600

    Add functions to the Point2 module
    Add functions to the Point2 module for performing operations on
    two-dimensional points.

--- lib/raytracer/point_2.ex
+++ lib/raytracer/point_2.ex
@@ -1,8 +1,64 @@
 defmodule Raytracer.Point2 do
+  alias Raytracer.Vector2
+
   defstruct [
     x: 0.0,
     y: 0.0,
   ]
 
   @type t :: %__MODULE__{x: float, y: float}
+
+  @spec abs(t) :: t
+  def abs(%__MODULE__{x: x, y: y}) do
+    %__MODULE__{x: Kernel.abs(x), y: Kernel.abs(y)}
+  end
+
+  @spec add(t, t | Vector2.t) :: t
+  def add(%__MODULE__{x: x1, y: y1}, %__MODULE__{x: x2, y: y2}) do
+    %__MODULE__{x: x1 + x2, y: y1 + y2}
+  end
+
+  def add(%__MODULE__{x: x, y: y}, %Vector2{dx: dx, dy: dy}) do
+    %__MODULE__{x: x + dx, y: y + dy}
+  end
+
+  @spec distance_between(t, t) :: float
+  def distance_between(point1, point2) do
+    point1 |> subtract(point2) |> Vector2.length
+  end
+
+  @spec distance_between_squared(t, t) :: float
+  def distance_between_squared(point1, point2) do
+    point1 |> subtract(point2) |> Vector2.length_squared
+  end
+
+  @spec divide(t, float) :: t
+  def divide(_point, scalar) when scalar == 0.0 do
+    raise ArgumentError, message: "scalar value 0 results in division by 0"
+  end
+
+  def divide(point, scalar) do
+    multiply(point, 1.0 / scalar)
+  end
+
+  @spec lerp(float, t, t) :: t
+  def lerp(t, point1, point2) do
+    point1
+    |> multiply(1 - t)
+    |> add(multiply(point2, t))
+  end
+
+  @spec multiply(t, float) :: t
+  def multiply(%__MODULE__{x: x, y: y}, scalar) do
+    %__MODULE__{x: x * scalar, y: y * scalar}
+  end
+
+  @spec subtract(t, t | Vector2.t) :: t | Vector2.t
+  def subtract(%__MODULE__{x: x, y: y}, %Vector2{dx: dx, dy: dy}) do
+    %__MODULE__{x: x - dx, y: y - dy}
+  end
+
+  def subtract(%__MODULE__{x: x1, y: y1}, %__MODULE__{x: x2, y: y2}) do
+    %Vector2{dx: x1 - x2, dy: y1 - y2}
+  end
 end
--- test/raytracer/point_2_test.exs
+++ test/raytracer/point_2_test.exs
@@ -1,3 +1,100 @@
 defmodule Raytracer.Point2Test do
   use ExUnit.Case, async: true
+
+  alias Raytracer.Point2
+  alias Raytracer.Vector2
+
+  describe "Raytracer.Point2.abs/1" do
+    test "returns the point with absolute value of each coordinate" do
+      p = %Point2{x: -1.0, y: -2.0}
+
+      assert Point2.abs(p) == %Point2{x: 1.0, y: 2.0}
+    end
+  end
+
+  describe "Raytracer.Point2.add/2" do
+    test "adds two points and returns the resulting point" do
+      p1 = %Point2{x: 1.0, y: 2.0}
+      p2 = %Point2{x: 4.0, y: 5.0}
+
+      assert Point2.add(p1, p2) == %Point2{x: 5.0, y: 7.0}
+    end
+
+    test "adds a point and a vector and returns the resulting point" do
+      p = %Point2{x: 1.0, y: 2.0}
+      v = %Vector2{dx: 4.0, dy: 5.0}
+
+      assert Point2.add(p, v) == %Point2{x: 5.0, y: 7.0}
+    end
+  end
+
+  describe "Raytracer.Point2.distance_between/2" do
+    test "computes the distance between two points" do
+      p1 = %Point2{x: 0.0, y: 0.0}
+      p2 = %Point2{x: 2.0, y: 0.0}
+
+      assert Point2.distance_between(p1, p2) == 2.0
+    end
+  end
+
+  describe "Raytracer.Point2.distance_between_squared/2" do
+    test "computes the squared distance between two points" do
+      p1 = %Point2{x: 0.0, y: 0.0}
+      p2 = %Point2{x: 2.0, y: 0.0}
+
+      assert Point2.distance_between_squared(p1, p2) == 4.0
+    end
+  end
+
+  describe "Raytracer.Point2.divide/2" do
+    test "divides a point by a scalar value and returns the resulting point" do
+      p = %Point2{x: 2.0, y: 4.0}
+      scalar = 2.0
+
+      assert Point2.divide(p, scalar) == %Point2{x: 1.0, y: 2.0}
+    end
+
+    test "raises an error when scalar is 0" do
+      p = %Point2{}
+      scalar = 0.0
+
+      assert_raise ArgumentError, fn ->
+        Point2.divide(p, scalar)
+      end
+    end
+  end
+
+  describe "Raytracer.Point2.lerp/3" do
+    test "linearly interpolates between two points" do
+      p1 = %Point2{x: 1.0, y: 1.0}
+      p2 = %Point2{x: -1.0, y: -1.0}
+
+      assert Point2.lerp(0.5, p1, p2) == %Point2{x: 0.0, y: 0.0}
+    end
+  end
+
+  describe "Raytracer.Point2.multiply/2" do
+    test "multiplies a point by a scalar value and returns the resulting point" do
+      p = %Point2{x: 2.0, y: 4.0}
+      scalar = 2.0
+
+      assert Point2.multiply(p, scalar) == %Point2{x: 4.0, y: 8.0}
+    end
+  end
+
+  describe "Raytracer.Point2.subtract/2" do
+    test "subtracts a vector from a point and returns the resulting point" do
+      p = %Point2{x: 1.0, y: 2.0}
+      v = %Vector2{dx: 4.0, dy: 5.0}
+
+      assert Point2.subtract(p, v) == %Point2{x: -3.0, y: -3.0}
+    end
+
+    test "subtracts two points and returns the resulting vector" do
+      p1 = %Point2{x: 1.0, y: 2.0}
+      p2 = %Point2{x: 4.0, y: 5.0}
+
+      assert Point2.subtract(p1, p2) == %Vector2{dx: -3.0, dy: -3.0}
+    end
+  end
 end
