df6873ae29e1bc88ed69fac609a36033b59af477
Jason Stewart <jason.r.stewart@gmail.com>
2016-11-20 17:59:07 -0600

    Add documentation to the Shape protocol

--- lib/raytracer/shape.ex
+++ lib/raytracer/shape.ex
@@ -1,12 +1,26 @@
 defprotocol Raytracer.Shape do
+  @moduledoc """
+  A protocol that defines an interface for working with shapes.
+  """
+
   alias Raytracer.Geometry.{Ray, Point3, Vector3}
 
+  @doc """
+  Computes the distance from the origin of `ray` to the intersection point of
+  `shape`. If `ray` and `shape` do not intersect `nil` is returned.
+  """
   @spec compute_intersection(t, Ray.t) :: nil | float
   def compute_intersection(shape, ray)
 
+  @doc """
+  Computes the inward pointing normal vector at `point` on `shape`.
+  """
   @spec compute_inward_normal(t, Point3.t) :: Vector3.t
   def compute_inward_normal(shape, point)
 
+  @doc """
+  Computes the outward pointing normal vector at `point` on `shape`.
+  """
   @spec compute_outward_normal(t, Point3.t) :: Vector3.t
   def compute_outward_normal(shape, point)
 end
