b3e9db1aeb491733193615d08d423de239f6a627
Jason Stewart <jason.r.stewart@gmail.com>
2016-11-20 16:44:23 -0600

    Add documentation to the Ray module
    Add documentation and alias __MODULE__ to the Ray module.

--- lib/raytracer/geometry/ray.ex
+++ lib/raytracer/geometry/ray.ex
@@ -1,19 +1,28 @@
 defmodule Raytracer.Geometry.Ray do
+  @moduledoc """
+  Three-dimensional ray represented by an origin point and a direction vector.
+  """
+
+  alias __MODULE__
   alias Raytracer.Geometry.{Point3, Vector3}
 
   defstruct [
-    origin: %Point3{x: 0.0, y: 0.0, z: 0.0},
-    direction: %Vector3{dx: 0.0, dy: 0.0, dz: 0.0},
+    origin: %Point3{},
+    direction: %Vector3{},
   ]
 
-  @type t :: %__MODULE__{origin: Point3.t, direction: Vector3.t}
+  @type t :: %Ray{origin: Point3.t, direction: Vector3.t}
 
-  @spec point_at(t, float) :: Point3.t
+  @doc """
+  Computes the point at `distance` on `ray`. An error is raised if `distance` is
+  less than 0.
+  """
+  @spec point_at(t, number) :: Point3.t
   def point_at(_ray, distance) when distance < 0 do
     raise ArgumentError, message: "negative value given for distance"
   end
 
-  def point_at(%__MODULE__{origin: origin, direction: direction}, distance) do
+  def point_at(%Ray{origin: origin, direction: direction}, distance) do
     direction
     |> Vector3.multiply(distance)
     |> Vector3.add(origin)
