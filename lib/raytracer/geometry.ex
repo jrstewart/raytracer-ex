410e15003f754a739b1a0a28c6e5d5a7542b9646
Jason Stewart <jason.r.stewart@gmail.com>
2016-11-22 14:52:25 -0600

    Add clamp function to the Geometry module
    Add clamp function to the Geometry module that allows a value to be
    clamped between given minimum and maximum values.

--- lib/raytracer/geometry.ex
+++ lib/raytracer/geometry.ex
@@ -4,15 +4,21 @@ defmodule Raytracer.Geometry do
   """
 
   @doc """
+  Check if `value` is between `min_value` and `max_value`. If `value` is less
+  than `min_value` then `min_value` is returned. If `value` is greater than
+  `max_value` then `max_value` is returned. Otherwise, `value` is returned.
+  """
+  @spec clamp(number, number, number) :: number
+  def clamp(value, min_value, _) when value < min_value, do: min_value
+  def clamp(value, _, max_value) when value > max_value, do: max_value
+  def clamp(value, _, _), do: value
+
+  @doc """
   Linearly interpolates between `value1` and `value2` based on the value of `t`.
   If `t` is 0, then `value1` is returned. if `t` is 1 then `value2` is returned.
   """
   @spec lerp(number, number, number) :: number
-  def lerp(value1, _value2, 0), do: value1
-
-  def lerp(_value1, value2, 1), do: value2
-
-  def lerp(value1, value2, t) do
-    (1 - t) * value1 + t * value2
-  end
+  def lerp(value1, _, 0), do: value1
+  def lerp(_, value2, 1), do: value2
+  def lerp(value1, value2, t), do: (1 - t) * value1 + t * value2
 end
--- test/raytracer/geometry_test.exs
+++ test/raytracer/geometry_test.exs
@@ -3,6 +3,20 @@ defmodule Raytracer.GeometryTest do
 
   alias Raytracer.Geometry
 
+  describe "Raytracer.Geometry.clamp/3" do
+    test "returns min_value if value is less than min_value" do
+      assert Geometry.clamp(-1.0, 0.0, 3.0) == 0.0
+    end
+
+    test "returns max_value if value is greater than max_value" do
+      assert Geometry.clamp(4.0, 0.0, 3.0) == 3.0
+    end
+
+    test "returns value if value is within min_value and max_value" do
+      assert Geometry.clamp(2.0, 0.0, 3.0) == 2.0
+    end
+  end
+
   describe "Raytracer.Geometry.lerp/3" do
     test "linearly interpolates between two values" do
       assert Geometry.lerp(0.0, 2.0, 0.5) == 1.0
