5bc903952f8dbd936974baf6406c727b21aa5042
Jason Stewart <jason.r.stewart@gmail.com>
2017-03-28 10:43:03 -0500

    Update the project dependencies
    Update the credo and dialyxir project dependecies. Add an ingore config
    file to specify dialyzer warnings that should be ignored.

--- mix.exs
+++ mix.exs
@@ -1,30 +1,48 @@
 defmodule Raytracer.Mixfile do
   use Mix.Project
 
+
+  @doc false
   def project do
     [
       app: :raytracer,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
       escript: escript_config(),
+      dialyzer: dialyzer_config(),
     ]
   end
 
-  def application do
-    [applications: [:logger]]
-  end
 
   defp deps do
     [
-      {:credo, "~> 0.5.2", only: [:dev]},
+      {:credo, "~> 0.7", only: [:dev]},
       {:dialyxir, "~> 0.4", only: [:dev]},
     ]
   end
 
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+
+  defp elixirc_paths(_), do: ["lib"]
+
+
   defp escript_config do
     [main_module: Raytracer]
+  end
+
+
+  defp dialyzer_config do
+    [ignore_warnings: "config/dialyzer.ignore-warnings"]
+  end
+
+
+  @doc false
+  def application do
+    [extra_applications: [:logger]]
   end
 end
