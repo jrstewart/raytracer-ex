109ad936ed8bf50c5225ac645a549eeaf618226c
Jason Stewart <jason.r.stewart@gmail.com>
2017-07-30 09:28:26 -0500

    Update the project dependencies
    Update credo to version 0.8.4 and dialyxir to version 0.5.1.

--- mix.exs
+++ mix.exs
@@ -23,13 +23,9 @@ defmodule Raytracer.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
-  defp escript_config do
-    [main_module: Raytracer]
-  end
+  defp escript_config, do: [main_module: Raytracer]
 
-  defp dialyzer_config do
-    [ignore_warnings: "config/dialyzer.ignore-warnings"]
-  end
+  defp dialyzer_config, do: [ignore_warnings: "config/dialyzer.ignore-warnings"]
 end
