2d152b654521a2d3354360bee7c0eccf0e0828c8
Jason Stewart <jason.r.stewart@gmail.com>
2017-07-30 12:10:07 -0500

    Refactor the TGAImage module
    Remove a few of the private helper functions and temporary variables
    from the TGAImage module.

--- lib/raytracer/tga_image.ex
+++ lib/raytracer/tga_image.ex
@@ -41,35 +41,26 @@ defmodule Raytracer.TGAImage do
   end
 
   defp file_data(image, pixels) do
-    header(image) <> format_pixels(image, pixels, "") <> footer()
-  end
-
-  defp header(image) do
     <<image.id_length::little-8>> <>
     <<image.color_map_type::little-8>> <>
     <<image.image_type::little-8>> <>
-    color_map_specification_to_binary(image.color_map_specification) <>
+    <<image.color_map_specification.first_entry_index::little-16>> <>
+    <<image.color_map_specification.num_entries::little-16>> <>
+    <<image.color_map_specification.entry_size::little-8>> <>
     <<image.x_origin::little-16>> <>
     <<image.y_origin::little-16>> <>
     <<image.width::little-16>> <>
     <<image.height::little-16>> <>
     <<image.pixel_depth::little-8>> <>
-    <<image.descriptor::little-8>>
-  end
-
-  defp color_map_specification_to_binary(color_map_specification) do
-    <<color_map_specification.first_entry_index::little-16>> <>
-    <<color_map_specification.num_entries::little-16>> <>
-    <<color_map_specification.entry_size::little-8>>
+    <<image.descriptor::little-8>> <>
+    format_pixels(image, pixels, "") <>
+    "TRUEVISION-XFILE." <>
+    :binary.copy(<<0>>, 9)
   end
 
   defp format_pixels(_, "", acc), do: acc
   defp format_pixels(%TGAImage{pixel_depth: pixel_depth} = image, <<pixel::24>> <> rest, acc)
        when pixel_depth == 24 do
     format_pixels(image, rest, acc <> <<pixel::little-24>>)
-  end
-
-  defp footer() do
-    "TRUEVISION-XFILE." <> :binary.copy(<<0>>, 9)
   end
 end
