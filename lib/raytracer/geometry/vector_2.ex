5ccf60610f45246fffbeba68be39f74b78f760fb
Jason Stewart <jason.r.stewart@gmail.com>
2017-11-19 04:24:30 -0600

    Add additional functions to the Vector2 module
    Add the remaining functions to the Vector2 module that exist in the
    Vector module.

--- lib/raytracer/geometry/vector_2.ex
+++ lib/raytracer/geometry/vector_2.ex
@@ -5,10 +5,51 @@ defmodule Raytracer.Geometry.Vector2 do
   """
 
   alias __MODULE__
+  alias Raytracer.Geometry.Point2
 
   defstruct dx: 0.0, dy: 0.0
 
   @type t :: %Vector2{dx: number, dy: number}
+
+  @doc """
+  Return a vector with the absolute value applied to each component of `vector`.
+  """
+  @spec abs(t) :: t
+  def abs(%Vector2{} = vector) do
+    %Vector2{dx: Kernel.abs(vector.dx), dy: Kernel.abs(vector.dy)}
+  end
+
+  @doc """
+  Adds two vectors returning the resulting vector or adds a vector and a point
+  returning the resulting point.
+  """
+  @spec add(t, t | Point2.t()) :: t | Point2.t()
+  def add(vector, vector_or_point)
+
+  def add(%Vector2{} = vector1, %Vector2{} = vector2) do
+    %Vector2{dx: vector1.dx + vector2.dx, dy: vector1.dy + vector2.dy}
+  end
+
+  def add(%Vector2{} = vector, %Point2{} = point) do
+    Point2.add(point, vector)
+  end
+
+  @doc """
+  Divides each of the components of `vector` by `scalar` and returns the
+  resulting vector.
+  """
+  @spec divide(t, number) :: t
+  def divide(%Vector2{} = vector, scalar) do
+    multiply(vector, 1.0 / scalar)
+  end
+
+  @doc """
+  Computes the dot product of two vectors.
+  """
+  @spec dot(t, t) :: number
+  def dot(%Vector2{} = vector1, %Vector2{} = vector2) do
+    vector1.dx * vector2.dx + vector1.dy * vector2.dy
+  end
 
   @doc """
   Computes the length of `vector`.
@@ -24,5 +65,54 @@ defmodule Raytracer.Geometry.Vector2 do
   @spec length_squared(t) :: number
   def length_squared(%Vector2{} = vector) do
     vector.dx * vector.dx + vector.dy * vector.dy
+  end
+
+  @doc """
+  Returns the component of `vector` with the largest value.
+  """
+  @spec max_component(t) :: number
+  def max_component(%Vector2{} = vector) do
+    max(vector.dx, vector.dy)
+  end
+
+  @doc """
+  Returns the component of `vector` with the smallest value.
+  """
+  @spec min_component(t) :: number
+  def min_component(%Vector2{} = vector) do
+    min(vector.dx, vector.dy)
+  end
+
+  @doc """
+  Multiplies each of the component of `vector` by `scalar` and returns the
+  resulting vector.
+  """
+  @spec multiply(t, number) :: t
+  def multiply(%Vector2{} = vector, scalar) do
+    %Vector2{dx: vector.dx * scalar, dy: vector.dy * scalar}
+  end
+
+  @doc """
+  Returns the vector pointing in the opposite direction of `vector`.
+  """
+  @spec negate(t) :: t
+  def negate(%Vector2{} = vector) do
+    %Vector2{dx: -vector.dx, dy: -vector.dy}
+  end
+
+  @doc """
+  Normalizes `vector` and returns the resulting vector.
+  """
+  @spec normalize(t) :: t
+  def normalize(%Vector2{} = vector) do
+    divide(vector, Vector2.length(vector))
+  end
+
+  @doc """
+  Subtracts two vectors and returns the resulting vector.
+  """
+  @spec subtract(t, t) :: t
+  def subtract(%Vector2{} = vector1, %Vector2{} = vector2) do
+    %Vector2{dx: vector1.dx - vector2.dx, dy: vector1.dy - vector2.dy}
   end
 end
--- test/raytracer/geometry/vector_2_test.exs
+++ test/raytracer/geometry/vector_2_test.exs
@@ -1,7 +1,49 @@
 defmodule Raytracer.Geometry.Vector2Test do
   use ExUnit.Case, async: true
 
-  alias Raytracer.Geometry.Vector2
+  alias Raytracer.Geometry.{Point2, Vector2}
+
+  describe "Raytracer.Geometry.Vector2.abs/1" do
+    test "returns the vector with absolute value of each coordinate" do
+      vector = %Vector2{dx: -1.0, dy: -2.0}
+
+      assert Vector2.abs(vector) == %Vector2{dx: 1.0, dy: 2.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.add/2" do
+    test "adds two vectors and returns the resulting vector" do
+      vector1 = %Vector2{dx: 1.0, dy: 2.0}
+      vector2 = %Vector2{dx: 4.0, dy: 5.0}
+
+      assert Vector2.add(vector1, vector2) == %Vector2{dx: 5.0, dy: 7.0}
+    end
+
+    test "adds a vector and a point and returns the resulting point" do
+      vector = %Vector2{dx: 4.0, dy: 5.0}
+      point = %Point2{x: 1.0, y: 2.0}
+
+      assert Vector2.add(vector, point) == %Point2{x: 5.0, y: 7.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.divide/2" do
+    test "divides a vector by a scalar value" do
+      vector = %Vector2{dx: 2.0, dy: 4.0}
+      scalar = 2.0
+
+      assert Vector2.divide(vector, scalar) == %Vector2{dx: 1.0, dy: 2.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.dot/2" do
+    test "computes the dot product of two vectors" do
+      vector1 = %Vector2{dx: 1.0, dy: 2.0}
+      vector2 = %Vector2{dx: 3.0, dy: 4.0}
+
+      assert Vector2.dot(vector1, vector2) == 11.0
+    end
+  end
 
   describe "Raytracer.Geometry.Vector2.length/1" do
     test "computes the length of a vector" do
@@ -16,6 +58,57 @@ defmodule Raytracer.Geometry.Vector2Test do
       vector = %Vector2{dx: 1.0, dy: 2.0}
 
       assert_in_delta Vector2.length_squared(vector), 5.0, 1.0e-7
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.max_component/1" do
+    test "returns the largest component value of a vector" do
+      assert Vector2.max_component(%Vector2{dx: 2.0, dy: 1.0}) == 2.0
+      assert Vector2.max_component(%Vector2{dx: 2.0, dy: 4.0}) == 4.0
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.min_component/1" do
+    test "returns the smallest component value of a vector" do
+      assert Vector2.min_component(%Vector2{dx: 1.0, dy: 2.0}) == 1.0
+      assert Vector2.min_component(%Vector2{dx: 2.0, dy: 1.0}) == 1.0
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.multiply/2" do
+    test "multiplies a point by a scalar value" do
+      vector = %Vector2{dx: 2.0, dy: 4.0}
+      scalar = 2.0
+
+      assert Vector2.multiply(vector, scalar) == %Vector2{dx: 4.0, dy: 8.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.negate/1" do
+    test "returns the vector pointing in the opposite direction of the given vector" do
+      vector = %Vector2{dx: 1.0, dy: 2.0}
+
+      assert Vector2.negate(vector) == %Vector2{dx: -1.0, dy: -2.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.normalize/1" do
+    test "normalizes a vector" do
+      vector = %Vector2{dx: 10.0, dy: 8.0}
+
+      result = Vector2.normalize(vector)
+
+      assert_in_delta result.dx, 0.78086880, 1.0e-7
+      assert_in_delta result.dy, 0.62469505, 1.0e-7
+    end
+  end
+
+  describe "Raytracer.Geometry.Vector2.subtract/2" do
+    test "subtracts two vectors" do
+      vector1 = %Vector2{dx: 1.0, dy: 5.0}
+      vector2 = %Vector2{dx: 2.0, dy: 3.0}
+
+      assert Vector2.subtract(vector1, vector2) == %Vector2{dx: -1.0, dy: 2.0}
     end
   end
 end
