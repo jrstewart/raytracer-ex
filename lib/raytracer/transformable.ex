8369200815eb1fb03e319318676b5925e835534b
Jason Stewart <jason.r.stewart@gmail.com>
2017-11-19 03:24:13 -0600

    Add the Transformable protocol
    Add the Transformable protocol that defines an interface for objects
    that can have a Transform applied to them.

---
+++ lib/raytracer/transformable.ex
@@ -0,0 +1,14 @@
+defprotocol Raytracer.Transformable do
+  @moduledoc """
+  A protocol that defines an interface for working with items that can have a
+  transform applied to them.
+  """
+
+  alias Raytracer.Transform
+
+  @doc """
+  Applies `transform` to `transformable` and returns the result.
+  """
+  @spec apply_transform(t, Transform.t()) :: t
+  def apply_transform(transformable, transform)
+end
