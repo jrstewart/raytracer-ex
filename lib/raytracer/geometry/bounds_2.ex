27efcb7db290fe8f6e62bf717a09e46e23d41c0d
Jason Stewart <jason.r.stewart@gmail.com>
2017-11-25 19:49:15 -0600

    Create the Bounds2 module
    Create the Bounds2 module that contains functionality for working with
    two dimensional bounding boxes.

---
+++ lib/raytracer/geometry/bounds_2.ex
@@ -0,0 +1,152 @@
+defmodule Raytracer.Geometry.Bounds2 do
+  @moduledoc """
+  This module provides functions for working with two dimensional axis-aligned
+  bounding boxes.
+  """
+
+  alias __MODULE__
+  alias Raytracer.Geometry
+  alias Raytracer.Geometry.{Point2, Vector2}
+
+  defstruct min: %Point2{}, max: %Point2{}
+
+  @type t :: %Bounds2{min: Point2.t(), max: Point2.t()}
+
+  @doc """
+  Return the corner point of `bounds` for the corner index equal to `index`.
+  """
+  @spec corner(t(), 0..3) :: Point2.t()
+  def corner(bounds, index)
+  def corner(%Bounds2{} = bounds, 0), do: bounds.min
+  def corner(%Bounds2{} = bounds, 1), do: %Point2{x: bounds.max.x, y: bounds.min.y}
+  def corner(%Bounds2{} = bounds, 2), do: %Point2{x: bounds.min.x, y: bounds.max.y}
+  def corner(%Bounds2{} = bounds, 3), do: bounds.max
+
+  @doc """
+  Compute the vector along the diagonal of `bounds` from the minimum point to
+  the maximum point.
+  """
+  @spec diagonal(t()) :: Vector2.t()
+  def diagonal(%Bounds2{} = bounds), do: Point2.subtract(bounds.max, bounds.min)
+
+  @doc """
+  Pads `bounds` in all directions by the value of `delta`.
+  """
+  @spec expand(t(), number()) :: t()
+  def expand(%Bounds2{} = bounds, delta) do
+    v = %Vector2{dx: delta, dy: delta}
+    %Bounds2{min: Point2.subtract(bounds.min, v), max: Point2.add(bounds.max, v)}
+  end
+
+  @doc """
+  Checks if `point` is inside `bounds`.
+  """
+  @spec inside?(t(), Point2.t()) :: boolean()
+  def inside?(%Bounds2{} = bounds, %Point2{} = point) do
+    point.x >= bounds.min.x && point.x <= bounds.max.x && point.y >= bounds.min.y &&
+      point.y <= bounds.max.y
+  end
+
+  @doc """
+  Checks if `point` is inside `bounds` excluding the upper boundary of `bounds`.
+  """
+  @spec inside_exclusive?(t(), Point2.t()) :: boolean()
+  def inside_exclusive?(%Bounds2{} = bounds, %Point2{} = point) do
+    point.x >= bounds.min.x && point.x < bounds.max.x && point.y >= bounds.min.y &&
+      point.y < bounds.max.y
+  end
+
+  @doc """
+  Compute the bounding box that is the intersection of `bounds1` and `bounds2`.
+  """
+  @spec intersect(t(), t()) :: t()
+  def intersect(%Bounds2{} = bounds1, %Bounds2{} = bounds2) do
+    %Bounds2{
+      min: %Point2{x: max(bounds1.min.x, bounds2.min.x), y: max(bounds1.min.y, bounds2.min.y)},
+      max: %Point2{x: min(bounds1.max.x, bounds2.max.x), y: min(bounds1.max.y, bounds2.max.y)}
+    }
+  end
+
+  @doc """
+  Linearly interpolates the point between the minimum and maximum corners of
+  `bounds` by the given amount in each direction specified by `tx` and `ty`
+  values.
+  """
+  @spec lerp(t(), number(), number()) :: Point2.t()
+  def lerp(%Bounds2{} = bounds, tx, ty) do
+    %Point2{
+      x: Geometry.lerp(bounds.min.x, bounds.max.x, tx),
+      y: Geometry.lerp(bounds.min.y, bounds.max.y, ty)
+    }
+  end
+
+  @doc """
+  Returns either `:x` or `:y` indicating the direction of the largest extent of
+  `bounds`.
+  """
+  @spec maximum_extent(t()) :: atom()
+  def maximum_extent(%Bounds2{} = bounds) do
+    bounds |> diagonal() |> find_largest_extent()
+  end
+
+  defp find_largest_extent(%Vector2{dx: dx, dy: dy}) when dx > dy, do: :x
+  defp find_largest_extent(_), do: :y
+
+  @doc """
+  Returns the continuous position of `point` relative to the minimum and maximum
+  corners of `bounds`. A point at the minimum corner has an offset of (0, 0) and
+  a point at the maximum corner has an offset of (1, 1).
+  """
+  @spec offset(t(), Point2.t()) :: {number(), number()}
+  def offset(%Bounds2{} = bounds, %Point2{} = point) do
+    {offset_x(bounds, point), offset_y(bounds, point)}
+  end
+
+  defp offset_x(bounds, point) do
+    if bounds.max.x > bounds.min.x do
+      (point.x - bounds.min.x) / (bounds.max.x - bounds.min.x)
+    else
+      point.x - bounds.min.x
+    end
+  end
+
+  defp offset_y(bounds, point) do
+    if bounds.max.y > bounds.min.y do
+      (point.y - bounds.min.y) / (bounds.max.y - bounds.min.y)
+    else
+      point.y - bounds.min.y
+    end
+  end
+
+  @doc """
+  Checks if `bounds1` and `bounds2` overlap.
+  """
+  @spec overlap?(t(), t()) :: boolean()
+  def overlap?(%Bounds2{} = bounds1, %Bounds2{} = bounds2) do
+    bounds1.max.x >= bounds2.min.x && bounds1.min.x <= bounds2.max.x &&
+      bounds1.max.y >= bounds2.min.y && bounds1.min.y <= bounds2.max.y
+  end
+
+  @doc """
+  Given a bounding box and a point, this function computes a new bounding box
+  that encompasses both the bounding box and point. Given two bounding boxes,
+  this function computes a new bounding box that bounds the space encompassed by
+  the two bounding boxes.
+  """
+  @spec union(t(), t() | Point2.t()) :: t()
+  def union(bounds, bounds_or_point)
+
+  def union(%Bounds2{} = bounds1, %Bounds2{} = bounds2) do
+    %Bounds2{
+      min: %Point2{x: min(bounds1.min.x, bounds2.min.x), y: min(bounds1.min.y, bounds2.min.y)},
+      max: %Point2{x: max(bounds1.max.x, bounds2.max.x), y: max(bounds1.max.y, bounds2.max.y)}
+    }
+  end
+
+  def union(%Bounds2{} = bounds, %Point2{} = point) do
+    %Bounds2{
+      min: %Point2{x: min(bounds.min.x, point.x), y: min(bounds.min.y, point.y)},
+      max: %Point2{x: max(bounds.max.x, point.x), y: max(bounds.max.y, point.y)}
+    }
+  end
+end
---
+++ test/raytracer/geometry/bounds_2_test.exs
@@ -0,0 +1,151 @@
+defmodule Raytracer.Geometry.Bounds2Test do
+  use ExUnit.Case, async: true
+
+  alias Raytracer.Geometry.{Bounds2, Point2, Vector2}
+
+  describe "Raytracer.Geometry.Bounds2.corner/2" do
+    test "returns the coordinates of the given corner index" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+
+      assert Bounds2.corner(bounds, 0) == %Point2{x: 0.0, y: 0.0}
+      assert Bounds2.corner(bounds, 1) == %Point2{x: 1.0, y: 0.0}
+      assert Bounds2.corner(bounds, 2) == %Point2{x: 0.0, y: 1.0}
+      assert Bounds2.corner(bounds, 3) == %Point2{x: 1.0, y: 1.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.diagonal/1" do
+    test "returns a vector pointing from the min point to the max point" do
+      bounds = %Bounds2{min: %Point2{x: -1.0, y: -1.0}, max: %Point2{x: 1.0, y: 1.0}}
+
+      assert Bounds2.diagonal(bounds) == %Vector2{dx: 2.0, dy: 2.0}
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.expand/2" do
+    test "expands a bounding box by the given delta value" do
+      delta = 2.0
+      bounds = %Bounds2{min: %Point2{x: -1.0, y: -1.0}, max: %Point2{x: 1.0, y: 1.0}}
+      expected = %Bounds2{min: %Point2{x: -3.0, y: -3.0}, max: %Point2{x: 3.0, y: 3.0}}
+
+      assert Bounds2.expand(bounds, delta) == expected
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.inside?/2" do
+    test "returns true if the point is inside the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 0.5, y: 0.5}
+
+      assert Bounds2.inside?(bounds, point)
+    end
+
+    test "returns false if the point is outside the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 2.0, y: 2.0}
+
+      refute Bounds2.inside?(bounds, point)
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.inside_exclusive?/2" do
+    test "returns true if the point is inside the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 0.5, y: 0.5}
+
+      assert Bounds2.inside_exclusive?(bounds, point)
+    end
+
+    test "returns false if the point is outside the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 2.0, y: 2.0}
+
+      refute Bounds2.inside_exclusive?(bounds, point)
+    end
+
+    test "returns false if the point is on the upper boundary of the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 1.0, y: 1.0}
+
+      refute Bounds2.inside_exclusive?(bounds, point)
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.intersect/2" do
+    test "create a new bounding box that is the intersection of the given bounding boxes" do
+      bounds1 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+      bounds2 = %Bounds2{min: %Point2{x: -2.0, y: -2.0}, max: %Point2{x: 1.0, y: 1.0}}
+      expected = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+
+      assert Bounds2.intersect(bounds1, bounds2) == expected
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.lerp/2" do
+    test "linearly interpolates between the corners of the box by the given amount in each direction" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+
+      assert Bounds2.lerp(bounds, 0.5, 0.25) == %Point2{x: 1.0, y: 0.5}
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.maximum_extent/1" do
+    test "returns an atom representing the axis with the largest value" do
+      bounds1 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 1.0}}
+      bounds2 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 2.0}}
+
+      assert Bounds2.maximum_extent(bounds1) == :x
+      assert Bounds2.maximum_extent(bounds2) == :y
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.offset/2" do
+    test "returns the continuous position of a point relative to the corners of the bounding box" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+      point = %Point2{x: 1.0, y: 0.5}
+
+      assert Bounds2.offset(bounds, point) == {0.5, 0.25}
+    end
+
+    test "when min point is greater than max point" do
+      bounds = %Bounds2{min: %Point2{x: 2.0, y: 2.0}, max: %Point2{x: 0.0, y: 0.0}}
+      point = %Point2{x: 1.0, y: 0.5}
+
+      assert Bounds2.offset(bounds, point) == {-1.0, -1.5}
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.overlap?/2" do
+    test "returns true if the two bounding boxes overlap" do
+      bounds1 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+      bounds2 = %Bounds2{min: %Point2{x: -2.0, y: -2.0}, max: %Point2{x: 1.0, y: 1.0}}
+
+      assert Bounds2.overlap?(bounds1, bounds2)
+    end
+
+    test "returns false if the two bounding boxes do not overlap" do
+      bounds1 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+      bounds2 = %Bounds2{min: %Point2{x: -2.0, y: -2.0}, max: %Point2{x: -1.0, y: -1.0}}
+
+      refute Bounds2.overlap?(bounds1, bounds2)
+    end
+  end
+
+  describe "Raytracer.Geometry.Bounds2.union/2" do
+    test "creates a new bounding box that encompasses the given bounding boxes" do
+      bounds1 = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      bounds2 = %Bounds2{min: %Point2{x: -1.0, y: -1.0}, max: %Point2{x: 0.0, y: 0.0}}
+      expected = %Bounds2{min: %Point2{x: -1.0, y: -1.0}, max: %Point2{x: 1.0, y: 1.0}}
+
+      assert Bounds2.union(bounds1, bounds2) == expected
+    end
+
+    test "creates a new bounding box that encompasses the given bounding box and point" do
+      bounds = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 1.0, y: 1.0}}
+      point = %Point2{x: 2.0, y: 2.0}
+      expected = %Bounds2{min: %Point2{x: 0.0, y: 0.0}, max: %Point2{x: 2.0, y: 2.0}}
+
+      assert Bounds2.union(bounds, point) == expected
+    end
+  end
+end
