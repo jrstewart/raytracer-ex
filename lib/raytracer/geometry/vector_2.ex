c6cd48feaa579c1f8a8ea3e953caf94e64f97af8
Jason Stewart <jason.r.stewart@gmail.com>
2019-03-20 07:30:43 -0500

    Add property-based testing to the Vector2 module
    Update the Vector2 tests to use property-based testing.

--- lib/raytracer/geometry/vector_2.ex
+++ lib/raytracer/geometry/vector_2.ex
@@ -1,7 +1,6 @@
 defmodule Raytracer.Geometry.Vector2 do
   @moduledoc """
-  This module provides a set of functions for working with two dimensional
-  vectors.
+  This module provides a set of functions for working with two dimensional vectors.
   """
 
   alias __MODULE__
@@ -9,110 +8,182 @@ defmodule Raytracer.Geometry.Vector2 do
 
   defstruct dx: 0.0, dy: 0.0
 
-  @type t :: %Vector2{dx: number(), dy: number()}
+  @type t :: %Vector2{dx: float, dy: float}
 
   @doc """
   Return a vector with the absolute value applied to each component of `vector`.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: -1.0, dy: -2.0}
+      iex> Vector2.abs(v)
+      %Vector2{dx: 1.0, dy: 2.0}
+
   """
-  @spec abs(t()) :: t()
-  def abs(%Vector2{} = vector) do
-    %Vector2{dx: Kernel.abs(vector.dx), dy: Kernel.abs(vector.dy)}
-  end
+  @spec abs(t) :: t
+  def abs(vector), do: %Vector2{dx: Kernel.abs(vector.dx), dy: Kernel.abs(vector.dy)}
 
   @doc """
-  Adds two vectors returning the resulting vector or adds a vector and a point
-  returning the resulting point.
+  Adds two vectors returning the resulting vector or adds a vector and a point returning the
+  resulting point.
+
+  ## Examples
+
+      iex> v1 = %Vector2{dx: 1.0, dy: 2.0}
+      iex> v2 = %Vector2{dx: 4.0, dy: 5.0}
+      iex> Vector2.add(v1, v2)
+      %Vector2{dx: 5.0, dy: 7.0}
+
+      iex> v = %Vector2{dx: 4.0, dy: 5.0}
+      iex> p = %Point2{x: 1.0, y: 2.0}
+      iex> Vector2.add(v, p)
+      %Point2{x: 5.0, y: 7.0}
+
   """
-  @spec add(t(), t() | Point2.t()) :: t() | Point2.t()
+  @spec add(t, t | Point2.t()) :: t | Point2.t()
   def add(vector, vector_or_point)
 
-  def add(%Vector2{} = vector1, %Vector2{} = vector2) do
-    %Vector2{dx: vector1.dx + vector2.dx, dy: vector1.dy + vector2.dy}
-  end
+  def add(vector1, %Vector2{} = vector2),
+    do: %Vector2{dx: vector1.dx + vector2.dx, dy: vector1.dy + vector2.dy}
 
-  def add(%Vector2{} = vector, %Point2{} = point) do
-    Point2.add(point, vector)
-  end
+  def add(vector, %Point2{} = point), do: Point2.add(point, vector)
 
   @doc """
-  Divides each of the components of `vector` by `scalar` and returns the
-  resulting vector.
+  Divides each of the components of `vector` by `scalar` and returns the resulting vector.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 2.0, dy: 4.0}
+      iex> scalar = 2.0
+      iex> Vector2.divide(v, scalar)
+      %Vector2{dx: 1.0, dy: 2.0}
+
   """
-  @spec divide(t(), number()) :: t()
-  def divide(%Vector2{} = vector, scalar) do
-    multiply(vector, 1.0 / scalar)
-  end
+  @spec divide(t, float) :: t
+  def divide(vector, scalar), do: multiply(vector, 1 / scalar)
 
   @doc """
   Computes the dot product of two vectors.
+
+  ## Examples
+
+      iex> v1 = %Vector2{dx: 1.0, dy: 2.0}
+      iex> v2 = %Vector2{dx: 3.0, dy: 4.0}
+      iex> Vector2.dot(v1, v2)
+      11.0
+
   """
-  @spec dot(t(), t()) :: number()
-  def dot(%Vector2{} = vector1, %Vector2{} = vector2) do
-    vector1.dx * vector2.dx + vector1.dy * vector2.dy
-  end
+  @spec dot(t, t) :: float
+  def dot(vector1, vector2), do: vector1.dx * vector2.dx + vector1.dy * vector2.dy
 
   @doc """
   Computes the length of `vector`.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 1.0, dy: 2.0}
+      iex> Vector2.length(v)
+      2.23606797749979
+
   """
-  @spec length(t()) :: number()
-  def length(%Vector2{} = vector) do
-    vector |> length_squared() |> :math.sqrt()
-  end
+  @spec length(t) :: float
+  def length(vector), do: vector |> length_squared() |> :math.sqrt()
 
   @doc """
   Computes the squared length of `vector`.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 1.0, dy: 2.0}
+      iex> Vector2.length_squared(v)
+      5.0
+
   """
-  @spec length_squared(t()) :: number()
-  def length_squared(%Vector2{} = vector) do
-    vector.dx * vector.dx + vector.dy * vector.dy
-  end
+  @spec length_squared(t) :: float
+  def length_squared(vector), do: vector.dx * vector.dx + vector.dy * vector.dy
 
   @doc """
   Returns the component of `vector` with the largest value.
+
+  ## Examples
+
+      iex> Vector2.max_component(%Vector2{dx: 2.0, dy: 1.0})
+      2.0
+
+      iex> Vector2.max_component(%Vector2{dx: 2.0, dy: 4.0})
+      4.0
+
   """
-  @spec max_component(t()) :: number()
-  def max_component(%Vector2{} = vector) do
-    max(vector.dx, vector.dy)
-  end
+  @spec max_component(t) :: float
+  def max_component(vector), do: max(vector.dx, vector.dy)
 
   @doc """
   Returns the component of `vector` with the smallest value.
+
+  ## Examples
+
+      iex> Vector2.min_component(%Vector2{dx: 1.0, dy: 2.0})
+      1.0
+
+      iex> Vector2.min_component(%Vector2{dx: 2.0, dy: 1.0})
+      1.0
+
   """
-  @spec min_component(t()) :: number()
-  def min_component(%Vector2{} = vector) do
-    min(vector.dx, vector.dy)
-  end
+  @spec min_component(t) :: float
+  def min_component(vector), do: min(vector.dx, vector.dy)
 
   @doc """
-  Multiplies each of the component of `vector` by `scalar` and returns the
-  resulting vector.
+  Multiplies each of the component of `vector` by `scalar` and returns the resulting vector.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 2.0, dy: 4.0}
+      iex> scalar = 2.0
+      iex> Vector2.multiply(v, scalar)
+      %Vector2{dx: 4.0, dy: 8.0}
+
   """
-  @spec multiply(t(), number()) :: t()
-  def multiply(%Vector2{} = vector, scalar) do
-    %Vector2{dx: vector.dx * scalar, dy: vector.dy * scalar}
-  end
+  @spec multiply(t, float) :: t
+  def multiply(vector, scalar), do: %Vector2{dx: vector.dx * scalar, dy: vector.dy * scalar}
 
   @doc """
   Returns the vector pointing in the opposite direction of `vector`.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 1.0, dy: 2.0}
+      iex> Vector2.negate(v)
+      %Vector2{dx: -1.0, dy: -2.0}
+
   """
-  @spec negate(t()) :: t()
-  def negate(%Vector2{} = vector) do
-    %Vector2{dx: -vector.dx, dy: -vector.dy}
-  end
+  @spec negate(t) :: t
+  def negate(vector), do: %Vector2{dx: -vector.dx, dy: -vector.dy}
 
   @doc """
   Normalizes `vector` and returns the resulting vector.
+
+  ## Examples
+
+      iex> v = %Vector2{dx: 10.0, dy: 8.0}
+      iex> Vector2.normalize(v)
+      %Vector2{dx: 0.7808688094430304, dy: 0.6246950475544243}
+
   """
-  @spec normalize(t()) :: t()
-  def normalize(%Vector2{} = vector) do
-    divide(vector, Vector2.length(vector))
-  end
+  @spec normalize(t) :: t
+  def normalize(vector), do: divide(vector, Vector2.length(vector))
 
   @doc """
   Subtracts two vectors and returns the resulting vector.
+
+  ## Examples
+
+      iex> v1 = %Vector2{dx: 1.0, dy: 5.0}
+      iex> v2 = %Vector2{dx: 2.0, dy: 3.0}
+      iex> Vector2.subtract(v1, v2)
+      %Vector2{dx: -1.0, dy: 2.0}
+
   """
-  @spec subtract(t(), t()) :: t()
-  def subtract(%Vector2{} = vector1, %Vector2{} = vector2) do
-    %Vector2{dx: vector1.dx - vector2.dx, dy: vector1.dy - vector2.dy}
-  end
+  @spec subtract(t, t) :: t
+  def subtract(vector1, vector2),
+    do: %Vector2{dx: vector1.dx - vector2.dx, dy: vector1.dy - vector2.dy}
 end
--- test/raytracer/geometry/vector_2_test.exs
+++ test/raytracer/geometry/vector_2_test.exs
@@ -1,114 +1,195 @@
 defmodule Raytracer.Geometry.Vector2Test do
   use ExUnit.Case, async: true
-
+  use PropCheck
+  import Raytracer.{GeometryTestHelpers, Generators}
   alias Raytracer.Geometry.{Point2, Vector2}
 
-  describe "Raytracer.Geometry.Vector2.abs/1" do
-    test "returns the vector with absolute value of each coordinate" do
-      vector = %Vector2{dx: -1.0, dy: -2.0}
+  @delta 1.0e-7
 
-      assert Vector2.abs(vector) == %Vector2{dx: 1.0, dy: 2.0}
+  doctest Vector2
+
+  describe "Raytracer.Geometry.Vector2.abs/1" do
+    property "computes the absolute value of the vector components" do
+      forall v <- vector2() do
+        result = Vector2.abs(v)
+        assert result.dx == if(v.dx >= 0.0, do: v.dx, else: -v.dx)
+        assert result.dy == if(v.dy >= 0.0, do: v.dy, else: -v.dy)
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.add/2" do
-    test "adds two vectors and returns the resulting vector" do
-      vector1 = %Vector2{dx: 1.0, dy: 2.0}
-      vector2 = %Vector2{dx: 4.0, dy: 5.0}
-
-      assert Vector2.add(vector1, vector2) == %Vector2{dx: 5.0, dy: 7.0}
+    property "subtracing one of the vectors from the resulting vector returns the other vector" do
+      forall {v1, v2} <- {vector2(), vector2()} do
+        result = Vector2.add(v1, v2)
+        assert_in_delta result.dx - v2.dx, v1.dx, @delta
+        assert_in_delta result.dy - v2.dy, v1.dy, @delta
+      end
     end
 
-    test "adds a vector and a point and returns the resulting point" do
-      vector = %Vector2{dx: 4.0, dy: 5.0}
-      point = %Point2{x: 1.0, y: 2.0}
-
-      assert Vector2.add(vector, point) == %Point2{x: 5.0, y: 7.0}
+    property "subtracing the vector from the resulting point returns the original point" do
+      forall {v, p} <- {vector2(), point2()} do
+        result = Vector2.add(v, p)
+        assert_in_delta result.x - v.dx, p.x, @delta
+        assert_in_delta result.y - v.dy, p.y, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.divide/2" do
-    test "divides a vector by a scalar value" do
-      vector = %Vector2{dx: 2.0, dy: 4.0}
-      scalar = 2.0
-
-      assert Vector2.divide(vector, scalar) == %Vector2{dx: 1.0, dy: 2.0}
+    property "multiplying the resulting vector by the scalar returns the original vector" do
+      forall {v, scalar} <- {vector2(), non_zero_float()} do
+        result = Vector2.divide(v, scalar)
+        assert_in_delta result.dx * scalar, v.dx, @delta
+        assert_in_delta result.dy * scalar, v.dy, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.dot/2" do
-    test "computes the dot product of two vectors" do
-      vector1 = %Vector2{dx: 1.0, dy: 2.0}
-      vector2 = %Vector2{dx: 3.0, dy: 4.0}
+    property "the dot product is commutative" do
+      forall {v1, v2} <- {vector2(), vector2()} do
+        assert_in_delta Vector2.dot(v1, v2), Vector2.dot(v2, v1), @delta
+      end
+    end
 
-      assert Vector2.dot(vector1, vector2) == 11.0
+    property "the dot product is distributive" do
+      forall {v1, v2, v3} <- {vector2(), vector2(), vector2()} do
+        result1 = Vector2.dot(v1, Vector2.add(v2, v3))
+        result2 = Vector2.dot(v1, v2) + Vector2.dot(v1, v3)
+        assert_in_delta result1, result2, @delta
+      end
+    end
+
+    property "the dot product is billinear" do
+      forall {v1, v2, v3, scalar} <- {vector2(), vector2(), vector2(), float()} do
+        result1 = Vector2.dot(v1, Vector2.add(Vector2.multiply(v2, scalar), v3))
+        result2 = scalar * Vector2.dot(v1, v2) + Vector2.dot(v1, v3)
+        assert_in_delta result1, result2, @delta
+      end
+    end
+
+    property "associative law for scalar and dot product" do
+      forall {v1, v2, scalar1, scalar2} <- {vector2(), vector2(), float(), float()} do
+        result1 = Vector2.dot(Vector2.multiply(v1, scalar1), Vector2.multiply(v2, scalar2))
+        result2 = scalar1 * scalar2 * Vector2.dot(v1, v2)
+        assert_in_delta result1, result2, @delta
+      end
+    end
+
+    property "the dot product of orthogonal vectors is 0" do
+      forall value <- float() do
+        v1 = %Vector2{dx: value, dy: 0.0}
+        v2 = %Vector2{dx: 0.0, dy: value}
+        assert_in_delta Vector2.dot(v1, v2), 0.0, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.length/1" do
-    test "computes the length of a vector" do
-      vector = %Vector2{dx: 1.0, dy: 2.0}
+    property "the length of the zero vector is 0" do
+      v = %Vector2{dx: 0.0, dy: 0.0}
+      assert Vector2.length(v) == 0.0
+    end
 
-      assert_in_delta Vector2.length(vector), 2.236067977, 1.0e-7
+    property "the length of a unit vector is 1" do
+      forall v <- vector2() do
+        v = Vector2.normalize(v)
+        assert_in_delta Vector2.length(v), 1.0, @delta
+      end
+    end
+
+    property "changing the direction of a vector does not change its length" do
+      forall v <- vector2() do
+        length1 = Vector2.length(v)
+        length2 = %Vector2{dx: v.dy, dy: v.dx} |> Vector2.length()
+        assert_in_delta length1, length2, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.length_squared/1" do
-    test "computes the squared length of a vector" do
-      vector = %Vector2{dx: 1.0, dy: 2.0}
+    property "the squared length of the zero vector is 0" do
+      v = %Vector2{dx: 0.0, dy: 0.0}
+      assert Vector2.length_squared(v) == 0.0
+    end
 
-      assert_in_delta Vector2.length_squared(vector), 5.0, 1.0e-7
+    property "the squared length of a unit vector is 1" do
+      forall v <- vector2() do
+        v = Vector2.normalize(v)
+        assert_in_delta Vector2.length_squared(v), 1.0, @delta
+      end
+    end
+
+    property "changing the direction of a vector does not change its squared length" do
+      forall v <- vector2() do
+        length1 = Vector2.length_squared(v)
+        length2 = %Vector2{dx: v.dy, dy: v.dx} |> Vector2.length_squared()
+        assert_in_delta length1, length2, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.max_component/1" do
-    test "returns the largest component value of a vector" do
-      assert Vector2.max_component(%Vector2{dx: 2.0, dy: 1.0}) == 2.0
-      assert Vector2.max_component(%Vector2{dx: 2.0, dy: 4.0}) == 4.0
+    property "returns the largest component value of a vector" do
+      forall v <- vector2() do
+        cond do
+          v.dx > v.dy ->
+            assert Vector2.max_component(v) == v.dx
+
+          true ->
+            assert Vector2.max_component(v) == v.dy
+        end
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.min_component/1" do
-    test "returns the smallest component value of a vector" do
-      assert Vector2.min_component(%Vector2{dx: 1.0, dy: 2.0}) == 1.0
-      assert Vector2.min_component(%Vector2{dx: 2.0, dy: 1.0}) == 1.0
+    property "returns the smallest component value of a vector" do
+      forall v <- vector2() do
+        cond do
+          v.dx < v.dy ->
+            assert Vector2.min_component(v) == v.dx
+
+          true ->
+            assert Vector2.min_component(v) == v.dy
+        end
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.multiply/2" do
-    test "multiplies a point by a scalar value" do
-      vector = %Vector2{dx: 2.0, dy: 4.0}
-      scalar = 2.0
-
-      assert Vector2.multiply(vector, scalar) == %Vector2{dx: 4.0, dy: 8.0}
-    end
-  end
-
-  describe "Raytracer.Geometry.Vector2.negate/1" do
-    test "returns the vector pointing in the opposite direction of the given vector" do
-      vector = %Vector2{dx: 1.0, dy: 2.0}
-
-      assert Vector2.negate(vector) == %Vector2{dx: -1.0, dy: -2.0}
+    property "dividing the resulting vector by the scalar returns the original vector" do
+      forall {v, scalar} <- {vector2(), non_zero_float()} do
+        result = Vector2.multiply(v, scalar)
+        assert_in_delta result.dx / scalar, v.dx, @delta
+        assert_in_delta result.dy / scalar, v.dy, @delta
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.normalize/1" do
-    test "normalizes a vector" do
-      vector = %Vector2{dx: 10.0, dy: 8.0}
+    property "a normalized vector has a length of 1" do
+      forall v <- vector2() do
+        assert_in_delta v |> Vector2.normalize() |> Vector2.length(), 1.0, @delta
+      end
+    end
 
-      result = Vector2.normalize(vector)
-
-      assert_in_delta result.dx, 0.78086880, 1.0e-7
-      assert_in_delta result.dy, 0.62469505, 1.0e-7
+    property "multiplying a normalized vector by its original length returns the original vector" do
+      forall v <- vector2() do
+        length = Vector2.length(v)
+        assert_equal_within_delta v |> Vector2.normalize() |> Vector2.multiply(length), v
+      end
     end
   end
 
   describe "Raytracer.Geometry.Vector2.subtract/2" do
-    test "subtracts two vectors" do
-      vector1 = %Vector2{dx: 1.0, dy: 5.0}
-      vector2 = %Vector2{dx: 2.0, dy: 3.0}
-
-      assert Vector2.subtract(vector1, vector2) == %Vector2{dx: -1.0, dy: 2.0}
+    property "adding the second vector to the resulting vector returns the first vector" do
+      forall {v1, v2} <- {vector2(), vector2()} do
+        result = Vector2.subtract(v1, v2)
+        assert_in_delta result.dx + v2.dx, v1.dx, @delta
+        assert_in_delta result.dy + v2.dy, v1.dy, @delta
+      end
     end
   end
 end
