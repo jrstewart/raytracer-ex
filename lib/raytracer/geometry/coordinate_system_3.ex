a342eef47db4d0944e604428a26c4c3515e54826
Jason Stewart <jason.r.stewart@gmail.com>
2019-08-27 10:57:16 -0600

    Add the create_from_vw function to the CoordinateSystem3 module
    Add the create_from_vw function to the COordinateSystem3 module for
    creating a coordinate system from either a single w vector or from a v/w
    pair of vectors.

--- lib/raytracer/geometry/coordinate_system_3.ex
+++ lib/raytracer/geometry/coordinate_system_3.ex
@@ -48,4 +48,54 @@ defmodule Raytracer.Geometry.CoordinateSystem3 do
 
     %CoordinateSystem3{u_axis: vector, v_axis: v_axis, w_axis: Vector3.cross(vector, v_axis)}
   end
+
+  @doc """
+  Creates a right-handed orthonormal coordinate system from either a single `w` vector or from the
+  `v` `w` pair of vectors.
+
+  This function is useful when creating the eye coordinate system axes from typical eye, center, up
+  view vectors. For example, eye-center would be used for `w`, and the up vector would be used for
+  `v`.
+
+  If `w` is a zero vector, then `u_axis`, `v_axis`, and `w_axis` of the coordinate system are set to
+  `(1.0, 0.0, 0.0)`, `(0.0, 1.0, 0.0)`, and `(0.0, 0.0, 1.0)` respectively. Otherwise, `w_axis` is
+  set to the normalized value of `w`, and `v_axis` is set to the component of `v` perpendicular to
+  `w`. If the resulting `v_axis` is the zero vector (which includes the case where the given `v` is
+  the zero vector), then `v_axis` is set to an arbitrary normalized vector perpendicular to `w`.
+  Finally, `u_axis` is the cross product of `v_axis` and `w_axis`.
+  """
+  @spec create_from_vw(Vector3.t(), Vector3.t()) :: t()
+  def create_from_vw(_v, %Vector3{dx: 0.0, dy: 0.0, dz: 0.0}) do
+    %CoordinateSystem3{
+      u_axis: %Vector3{dx: 1.0, dy: 0.0, dz: 0.0},
+      v_axis: %Vector3{dx: 0.0, dy: 1.0, dz: 0.0},
+      w_axis: %Vector3{dx: 0.0, dy: 0.0, dz: 1.0}
+    }
+  end
+
+  def create_from_vw(v, w) do
+    v = if v == Vector3.zero(), do: compute_arbitrary_normal(w), else: v
+    w_axis = Vector3.normalize(w)
+    {_, v_perp} = w |> Vector3.normalize() |> Vector3.decompose(v)
+
+    v_axis =
+      cond do
+        v_perp == Vector3.zero() ->
+          compute_arbitrary_normal(w)
+
+        Vector3.perpendicular?(v, w) ->
+          v
+
+        true ->
+          v_perp
+      end
+      |> Vector3.normalize()
+
+    u_axis = v_axis |> Vector3.cross(w_axis) |> Vector3.normalize()
+    %CoordinateSystem3{u_axis: u_axis, v_axis: v_axis, w_axis: w_axis}
+  end
+
+  defp compute_arbitrary_normal(vector) do
+    %Vector3{dx: 1.0 / vector.dx, dy: -1.0 / vector.dy, dz: 0.0}
+  end
 end
